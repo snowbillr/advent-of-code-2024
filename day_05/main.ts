23fe8453e5410fd5550761ee8df82ac7ac1e4262
Bill Reed <bill.reed@babylist.com>
2024-12-05 21:39:24 -0500

    refactor in anticipation of part 2

--- day_05/main.ts
+++ day_05/main.ts
@@ -8,34 +8,33 @@ enable()
 const exampleInput = readLines('day_05/example.txt');
 const dataInput = readLines('day_05/data.txt');
 
+function isOrdered(pages: number[], orderingRules: number[][]) {
+  const relevantOrderingRules = orderingRules.filter(orderingRules => orderingRules.every(rule => pages.includes(rule)));
+  return relevantOrderingRules.reduce((isOrdered, rule) => {
+    return isOrdered && pages.indexOf(rule[0]) < pages.indexOf(rule[1])
+  }, true)
+}
+
+function sumMiddlePages(pages: number[][]) {
+  return pages.reduce((sum, pages) => {
+    return sum + pages[Math.floor(pages.length / 2)]
+  }, 0);
+}
+
 solution('part 1 example', () => {
   const inputParser = InputParser.fromLines(exampleInput);
 
-  const alreadyOrderedUpdates = inputParser.pagesToUpdate.filter(pages => {
-    const relevantOrderingRules = inputParser.orderingRules.filter(orderingRules => orderingRules.every(rule => pages.includes(rule)));
-    return relevantOrderingRules.reduce((isOrdered, rule) => {
-      return isOrdered && pages.indexOf(rule[0]) < pages.indexOf(rule[1])
-    }, true)
-  })
+  const alreadyOrderedUpdates = inputParser.pagesToUpdate.filter(pages => isOrdered(pages, inputParser.orderingRules))
 
-  return alreadyOrderedUpdates.reduce((sum, pages) => {
-    return sum + pages[Math.floor(pages.length / 2)]
-  }, 0);
+  return sumMiddlePages(alreadyOrderedUpdates);
 })
 
 solution('part 1', () => {
   const inputParser = InputParser.fromLines(dataInput);
 
-  const alreadyOrderedUpdates = inputParser.pagesToUpdate.filter(pages => {
-    const relevantOrderingRules = inputParser.orderingRules.filter(orderingRules => orderingRules.every(rule => pages.includes(rule)));
-    return relevantOrderingRules.reduce((isOrdered, rule) => {
-      return isOrdered && pages.indexOf(rule[0]) < pages.indexOf(rule[1])
-    }, true)
-  })
+  const alreadyOrderedUpdates = inputParser.pagesToUpdate.filter(pages => isOrdered(pages, inputParser.orderingRules))
 
-  return alreadyOrderedUpdates.reduce((sum, pages) => {
-    return sum + pages[Math.floor(pages.length / 2)]
-  }, 0);
+  return sumMiddlePages(alreadyOrderedUpdates);
 })
 
 printSolutions()
