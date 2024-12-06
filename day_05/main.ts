e098302538d1bb02d6ea5466633e605a9917ac91
Bill Reed <bill.reed@babylist.com>
2024-12-05 21:51:18 -0500

    day 5 part 2

--- day_05/main.ts
+++ day_05/main.ts
@@ -21,6 +21,38 @@ function sumMiddlePages(pages: number[][]) {
   }, 0);
 }
 
+function checkRule(pages: number[], rule: number[]) {
+  return pages.indexOf(rule[0]) < pages.indexOf(rule[1]);
+}
+
+function sort(pages: number[], orderingRules: number[][]) {
+  const relevantOrderingRules = orderingRules.filter(orderingRules => orderingRules.every(rule => pages.includes(rule)));
+  const sortedPages = [...pages];
+
+  let hasSwapped = true;
+  while (hasSwapped === true) {
+    hasSwapped = false;
+
+    for (let rule of relevantOrderingRules) {
+      if (!checkRule(sortedPages, rule)) {
+        const [a, b] = rule;
+        const aIndex = sortedPages.indexOf(a);
+        const bIndex = sortedPages.indexOf(b);
+
+        sortedPages[aIndex] = b;
+        sortedPages[bIndex] = a;
+
+        hasSwapped = true;
+      }
+    }
+  }
+
+  log(pages, sortedPages)
+
+  return sortedPages;
+
+}
+
 solution('part 1 example', () => {
   const inputParser = InputParser.fromLines(exampleInput);
 
@@ -35,6 +67,24 @@ solution('part 1', () => {
   const alreadyOrderedUpdates = inputParser.pagesToUpdate.filter(pages => isOrdered(pages, inputParser.orderingRules))
 
   return sumMiddlePages(alreadyOrderedUpdates);
+})
+
+solution('part 2 example', () => {
+  const inputParser = InputParser.fromLines(exampleInput);
+
+  const unsortedPages = inputParser.pagesToUpdate.filter(pages => !isOrdered(pages, inputParser.orderingRules))
+  const sortedUpdates = unsortedPages.map(pages => sort(pages, inputParser.orderingRules))
+
+  return sumMiddlePages(sortedUpdates);
+})
+
+solution('part 2 data', () => {
+  const inputParser = InputParser.fromLines(dataInput);
+
+  const unsortedPages = inputParser.pagesToUpdate.filter(pages => !isOrdered(pages, inputParser.orderingRules))
+  const sortedUpdates = unsortedPages.map(pages => sort(pages, inputParser.orderingRules))
+
+  return sumMiddlePages(sortedUpdates);
 })
 
 printSolutions()
